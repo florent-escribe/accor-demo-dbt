99ec8c8122819e028b8bf2e33323b1cdbd99047e
Florent Escribe <florent.escribe@etu.minesparis.psl.eu>
2024-01-18 16:09:31 +0100

    feat(lint/format): Add SqlFluff Linter and Formatter

--- models/top_hotel_sales.sql
+++ models/top_hotel_sales.sql
@@ -1,4 +1,7 @@
-SELECT Hotel, Country, SUM(Sales) AS TotalSales
+SELECT
+    Hotel,
+    Country,
+    SUM(Sales) AS TotalSales
 FROM {{ source('hotel_sales', 'HOTEL_SALES_TABLE') }}
 GROUP BY Hotel, Country
 ORDER BY TotalSales DESC
--- tests/test_total_sales_positive.sql
+++ tests/test_total_sales_positive.sql
@@ -1,3 +1,1 @@
-select *
-from {{ref('top_hotel_sales')}} 
-where TotalSales <= 0
+select * from {{ ref('top_hotel_sales') }} where TotalSales <= 0
